1ce0bf97ed24d386ec1c4285f0b5593f2375b495
mennitise <mennitise@gmail.com>
2019-06-02 18:17:58 -0300

    upload exercises 1 & 4

---
+++ ejercicio_4_1-sydney_ISP.py
@@ -0,0 +1,46 @@
+import pybgpstream
+import pprint
+import numpy as np
+
+#---------------------------------------------------------
+
+# Definitions
+TIME_INIT = "2012-03-01"
+TIME_END = "2012-03-01 00:05"
+
+COLLECTOR = 'route-views.sydney'
+
+# AS target
+# ASN: 8966
+# ASname: Etisalat
+
+# ISP
+targetAS = 2914
+
+#---------------------------------------------------------
+
+# Query configuration
+stream = pybgpstream.BGPStream(
+		from_time=TIME_INIT,
+		until_time=TIME_END,
+		filter="type ribs and collector %s and path %s"%(COLLECTOR,targetAS))
+
+#---------------------------------------------------------
+
+# Execution & parse of the query
+
+print ''
+print 'ASPATHS to the target AS:'
+print '-------------------------'
+print ''
+
+index = 0
+ASPATHs = []
+ASPATHS_v=[]
+for elem in stream:
+	if str(elem.fields['as-path'].split(' ')[-1]) == str(targetAS):
+		if elem.fields['as-path'] not in ASPATHs:
+			index += 1
+			ASPATHs.append(elem.fields['as-path'])
+			print str(index)+' - '+elem.fields['as-path']
+			ASPATHS_v.append(np.array(elem.fields["as-path"].split(' ')).astype(int))
